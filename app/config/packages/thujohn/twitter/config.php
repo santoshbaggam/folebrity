4d2810b660f215a9fb13cdcb2802d6a996d53fa3
Santosh Baggam <santosh.0709@gmail.com>
2015-03-31 22:10:47 +0530

    fix thujohn config

--- app/config/packages/thujohn/twitter/config.php
+++ app/config/packages/thujohn/twitter/config.php
@@ -1,14 +1,21 @@
 <?php
 
-// You can find the keys here : https://dev.twitter.com/
+// You can find the keys here : https://apps.twitter.com/
 
-return array(
+return [
+	'debug'               => false,
+
 	'API_URL'             => 'api.twitter.com',
+	'UPLOAD_URL'          => 'upload.twitter.com',
 	'API_VERSION'         => '1.1',
+	'AUTHENTICATE_URL'    => 'https://api.twitter.com/oauth/authenticate',
+	'AUTHORIZE_URL'       => 'https://api.twitter.com/oauth/authorize',
+	'ACCESS_TOKEN_URL'    => 'https://api.twitter.com/oauth/access_token',
+	'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
 	'USE_SSL'             => true,
 
 	'CONSUMER_KEY'        => getenv('twitter.consumer_key'),
-	'CONSUMER_SECRET'     => getenv('twitter.consumer_secret'),
+	'CONSUMER_SECRET'     => getenv('twitter.consumer_key'),
 	'ACCESS_TOKEN'        => '',
 	'ACCESS_TOKEN_SECRET' => '',
-);
+];
